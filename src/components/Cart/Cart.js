5d6bea2bd63fad0b3c784d62a9879d514b1a80f1
Iqtidar102011 <iqidarwasif@gmail.comm>
2022-09-27 21:58:10 +0200

    cart calculation added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({ cart }) => {
+    const quantity = cart.reduce((previous, current) => previous + current.quantity, 0)
+
+    const total = cart.reduce((previous, current) => previous + current.price * current.quantity, 0)
+    // console.log(quantity, total)
+    const shipping = cart.reduce((previous, cart) => previous + cart.shipping, 0)
+    const taxString = (total * 0.1).toFixed(2)
+    const tax = parseFloat(taxString)
+    const grandTotal = total + shipping + tax;
     return (
-        <div>
+        <div className='cart'>
             <h2>Order Summery</h2>
-            <p>Selected items : {cart.length}</p>
+            <p>Selected items : {quantity}</p>
+            <p>Total price:${total}</p>
+            <p>Shipping:${shipping}</p>
+            <p>Tax:${tax}</p>
+            <h4>Grand Total:${grandTotal.toFixed(2)}</h4>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -11,10 +12,40 @@ const Shop = () => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProduct(data))
-    }, [])
-    const handleAddToCart = product => {
-        const newCart = [...cart, product];
+    }, []);
+
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        let savedCart = [];
+        // storedcart is an object
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id)
+            if (addedProduct) {
+                // if addedproduct is truthyvalye set the quantity as property value of id
+                // step 1 .. set quantity 
+                const quantity = storedCart[id];
+                // addedProduct is an object which has a property named quantity. so, setting the property name (addedproduct.quantity=)
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct)
+            }
+        }
+        setCart(savedCart);
+    }, [products])
+
+    const handleAddToCart = selected => {
+        let newCart = [];
+        const exists = cart.find(product => product.id === selected.id)
+        if (!exists) {
+            selected.quantity = 1;
+            newCart = [...cart, selected]
+        }
+        else {
+            const rest = cart.filter(product => product.id !== selected.id)
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists]
+        }
         setCart(newCart);
+        addToDb(selected.id)
     }
     return (
         <div className='shop-container'>
